7c8a87231ff672b038c397e6fcd9ccb8ce68f395
Michel.Benedetti <Michel.Benedetti@15e89361-3e06-f125-878a-7c0779239558>
2012-03-18 16:42:23 +0000

    contatore delle pagine

--- Philinternational/Philinternational/Lotti/elencoLotto.aspx.designer.cs
+++ Philinternational/Philinternational/Lotti/elencoLotto.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Philinternational {
         protected global::System.Web.UI.WebControls.Literal testoTitle;
         
         /// <summary>
+        /// metaDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal metaDescription;
+        
+        /// <summary>
         /// navigazioneOutput control.
         /// </summary>
         /// <remarks>
