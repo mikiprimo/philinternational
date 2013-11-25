a780b0ba4b060ee2c872b865fdb15c73c5c63d20
michel.benedetti@gmail.com <michel.benedetti@gmail.com@15e89361-3e06-f125-878a-7c0779239558>
2013-11-25 23:42:05 +0000

    Aggiornamento meta tag description aggioramento h1, h2

--- trunk/Philinternational/Philinternational/Lotti/offerta.aspx.designer.cs
+++ trunk/Philinternational/Philinternational/Lotti/offerta.aspx.designer.cs
@@ -22,6 +22,24 @@ namespace Philinternational {
         protected global::System.Web.UI.WebControls.Literal testoTitle;
         
         /// <summary>
+        /// metaDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal metaDescription;
+        
+        /// <summary>
+        /// keyWords control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal keyWords;
+        
+        /// <summary>
         /// testoH1 control.
         /// </summary>
         /// <remarks>
